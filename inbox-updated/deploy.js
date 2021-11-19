2ec90a2fbdfca2b280bfc1dc0c8541c522387a76
Bileonaire <kiokoleon@gmail.com>
2021-11-19 17:02:01 +0300

    Update Project

--- inbox-updated/deploy.js
+++ inbox-updated/deploy.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
     'surprise siege now sketch ready bubble garbage chuckle salt entire increase little',
@@ -11,12 +12,15 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-    console.log('account -- ', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi']})
-        .send({ gas: '1000000', from: accounts[0]});
+    console.log('Attempting to deploy from account', accounts[0]);
 
-    console.log('address -- ', result.options.address);
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
 };
+
 deploy();
